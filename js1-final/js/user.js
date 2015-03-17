b48eefc38471e59235d6cfafbe0cea1b9f2a9aec
arturoceballos <aceballos118@gmail.com>
2015-03-16 20:16:49 -0700

    created user.js

---
+++ js1-final/js/user.js
@@ -0,0 +1,15 @@
+/**
+ * Created by arturoceballos on 3/16/15.
+ */
+
+(function(){
+    var User = function(first, last, username, password, email, dob, gender) {
+        this.first = first;
+        this.last = last;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.dob = dob;
+        this.gender = gender;
+    }
+}());
